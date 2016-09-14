f4bef27abf4edad65623606869a7dc707313e5ec
Hugo Bessa <hugo@badoque.com>
2016-09-14 10:13:11 -0300

    Adding setup.py and version

---
+++ bdq_utils/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BdqUtilsConfig(AppConfig):
+    name = 'bdq_utils'
---
+++ setup.py
@@ -0,0 +1,85 @@
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+import re
+import os
+import sys
+
+
+package = 'bdq_utils'
+
+requirements = [
+    'Django>=1.7',
+    'celery==3.1.23',
+    'django-celery==3.1.17',
+]
+
+
+def get_version(package):
+    """
+    Return package version as listed in `__version__` in `init.py`.
+    """
+    init_py = open(os.path.join(package, '__init__.py')).read()
+    return re.search("^__version__ = ['\"]([^'\"]+)['\"]", init_py, re.MULTILINE).group(1)
+
+
+def get_packages(package):
+    """
+    Return root package and all sub-packages.
+    """
+    return [dirpath
+            for dirpath, dirnames, filenames in os.walk(package)
+            if os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+
+def get_package_data(package):
+    """
+    Return all files under the root package, that are not in a
+    package themselves.
+    """
+    walk = [(dirpath.replace(package + os.sep, '', 1), filenames)
+            for dirpath, dirnames, filenames in os.walk(package)
+            if not os.path.exists(os.path.join(dirpath, '__init__.py'))]
+
+    filepaths = []
+    for base, filenames in walk:
+        filepaths.extend([os.path.join(base, filename)
+                          for filename in filenames])
+    return {package: filepaths}
+
+# python setup.py register
+if sys.argv[-1] == 'publish':
+    os.system("python setup.py sdist upload")
+    args = {'version': get_version(package)}
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %(version)s -m 'version %(version)s'" % args)
+    print("  git push --tags")
+    sys.exit()
+
+setup(
+    name='django-bdq-utils',
+    version=get_version(package),
+    url='http://github.com/badoque/django-bdq-utils',
+    license='MIT',
+    description='General utilities for working with rest_framework and celery',
+    author='Hugo Bessa',
+    author_email='hugo@badoque.com',
+    packages=get_packages(package),
+    package_data=get_package_data(package),
+    install_requires=requirements,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'License :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
