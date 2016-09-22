f778500982ceb4ba5d3b3972018026d741404725
Hugo Bessa <hugo@badoque.com>
2016-09-22 18:37:05 -0300

    Fixing app settings

--- bdq_utils/app_settings.py
+++ bdq_utils/app_settings.py
@@ -4,6 +4,4 @@ import importlib
 
 BDQ_UTILS = getattr(settings, 'BDQ_UTILS', {})
 
-send_mail_task = importlib.import_module({
-    'name': BDQ_UTILS.get('SEND_MAIL_TASK', 'bdq_utils.tasks.send_mail_task'),
-})
+send_mail_task = importlib.import_module(BDQ_UTILS.get('SEND_MAIL_TASK', 'bdq_utils.tasks.send_mail_task'))
